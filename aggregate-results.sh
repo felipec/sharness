32ab620c522674daf56d4a88122fae7258684c90
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-02-06 20:08:06 +0100

    Update Copyright notices to 2012

--- aggregate-results.sh
+++ aggregate-results.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (c) 2008-2011 Git project
+# Copyright (c) 2008-2012 Git project
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
