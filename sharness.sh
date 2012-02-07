43a825e676cbf3e3808e5c3e85e772ad6ad4c9c1
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-02-07 22:23:34 +0100

    Tomdoc test_debug

--- sharness.sh
+++ sharness.sh
@@ -222,6 +222,20 @@ test_known_broken_failure_() {
 	say_color skip "not ok $test_count - $@ # TODO known breakage"
 }
 
+# Public: Execute commands in debug mode.
+#
+# Takes a single argument and evaluates it only when the test script is started
+# with --debug. This is primarily meant for use during the development of test
+# scripts.
+#
+# $1 - Commands to be executed.
+#
+# Examples
+#
+#   test_debug "cat some_log_file"
+#
+# Returns the exit code of the last command executed in debug mode or 0
+#   otherwise.
 test_debug() {
 	test "$debug" = "" || eval "$1"
 }
