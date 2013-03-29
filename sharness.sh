cfb8f8fdd805356f4c3e1988015865f974c069a0
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-03-29 13:13:40 +0100

    Make test number come first in 'not ok $count - $message'
    See https://github.com/git/git/commit/5e5c006eb713b785c6761c32b907e02abc277f9a

--- sharness.sh
+++ sharness.sh
@@ -225,7 +225,7 @@ test_ok_() {
 
 test_failure_() {
 	test_failure=$(($test_failure + 1))
-	say_color error "not ok - $test_count $1"
+	say_color error "not ok $test_count - $1"
 	shift
 	echo "$@" | sed -e 's/^/#	/'
 	test "$immediate" = "" || { EXIT_OK=t; exit 1; }
