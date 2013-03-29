1af97de0fe226a0198d3d3660a08000a9e16cf28
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-03-29 13:50:32 +0100

    Paint known breakages in yellow and unexpectedly fixed known breakages in bold red
    https://github.com/git/git/commit/b73d9a2363beedd59c212b5aa6b9db2977c38b2b
    https://github.com/git/git/commit/e8e5195573bc0f314ccc288f7d009b47e4440760

--- sharness.sh
+++ sharness.sh
@@ -86,6 +86,7 @@ if test -n "$color"; then
 		case "$1" in
 			error) tput bold; tput setaf 1;; # bold red
 			skip)  tput bold; tput setaf 2;; # bold green
+			warn)  tput setaf 3;;            # brown/yellow
 			pass)  tput setaf 2;;            # green
 			info)  tput setaf 3;;            # brown
 			*) test -n "$quiet" && return;;
@@ -233,12 +234,12 @@ test_failure_() {
 
 test_known_broken_ok_() {
 	test_fixed=$(($test_fixed + 1))
-	say_color "" "ok $test_count - $@ # TODO known breakage"
+	say_color error "ok $test_count - $@ # TODO known breakage vanished"
 }
 
 test_known_broken_failure_() {
 	test_broken=$(($test_broken + 1))
-	say_color skip "not ok $test_count - $@ # TODO known breakage"
+	say_color warn "not ok $test_count - $@ # TODO known breakage"
 }
 
 # Public: Execute commands in debug mode.
@@ -585,19 +586,26 @@ test_done() {
 	fi
 
 	if test "$test_fixed" != 0; then
-		say_color pass "# fixed $test_fixed known breakage(s)"
+		say_color error "# $test_fixed known breakage(s) vanished; please update test(s)"
 	fi
 	if test "$test_broken" != 0; then
-		say_color error "# still have $test_broken known breakage(s)"
-		msg="remaining $(($test_count - $test_broken)) test(s)"
+		say_color warn "# still have $test_broken known breakage(s)"
+	fi
+	if test "$test_broken" != 0 || test "$test_fixed" != 0; then
+		test_remaining=$(( $test_count - $test_broken - $test_fixed ))
+		msg="remaining $test_remaining test(s)"
 	else
+		test_remaining=$test_count
 		msg="$test_count test(s)"
 	fi
+
 	case "$test_failure" in
 	0)
 		[ -z "$skip_all" ] || skip_all=" # SKIP $skip_all"
 
-		say_color pass "# passed all $msg"
+		if test $test_remaining -gt 0; then
+			say_color pass "# passed all $msg"
+		fi
 		say "1..$test_count$skip_all"
 
 		test -d "$remove_trash" &&
