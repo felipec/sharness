felipec/sharness

commit c11923ae253700d16c46765a09fb6d50cc6de263

make: add install and uninstall targets

Makefile
@@ -1,6 +1,27 @@
-TOMDOCSH := tomdoc.sh
+prefix = $(HOME)
+
+INSTALL_DIR = $(prefix)/share/sharness
+DOC_DIR = $(prefix)/share/doc/sharness
+EXAMPLE_DIR = $(DOC_DIR)/examples
+
+INSTALL_FILES = aggregate-results.sh sharness.sh
+DOC_FILES = API.md COPYING HISTORY.md README.git README.md
+EXAMPLE_FILES = test/Makefile test/simple.t
+
+INSTALL = install
+RM = rm -f
+TOMDOCSH = tomdoc.sh
 
 all:
+
+install: all
+	$(INSTALL) -d -m 755 $(INSTALL_DIR) $(DOC_DIR) $(EXAMPLE_DIR)
+	$(INSTALL) -m 644 $(INSTALL_FILES) $(INSTALL_DIR)
+	$(INSTALL) -m 644 $(DOC_FILES) $(DOC_DIR)
+	$(INSTALL) -m 644 $(EXAMPLE_FILES) $(EXAMPLE_DIR)
+
+uninstall:
+	$(RM) -r $(INSTALL_DIR) $(DOC_DIR) $(EXAMPLE_DIR)
 
 doc: all
 	{ printf "# Sharness API\n\n"; \
@@ -10,4 +31,4 @@ doc: all
 test: all
 	$(MAKE) -C test
 
-.PHONY: all doc test
+.PHONY: all install uninstall doc test

commit 32d735fc8a4aa77dbaed51bc6a824679dcb904f4

Makefile: Allow prefix to be set from outside
This way external build systems can override prefix and decide to not
install into $HOME.

Signed-off-by: Alexander Dahl <ada@thorsis.com>
Fixes: c11923ae2537 ("make: add install and uninstall targets")

Makefile
@@ -1,4 +1,4 @@
-prefix = $(HOME)
+prefix ?= $(HOME)
 
 INSTALL_DIR = $(prefix)/share/sharness
 DOC_DIR = $(prefix)/share/doc/sharness