felipec/sharness

commit 96e0ea2c7b05e9570c82058f74a1e3557b7d09cd

Improve zsh source method
$0 fails if the NO_FUNCTION_ARGZERO option is set.

Additionally, %x would correctly return the name of the sourced script
even inside a function, even though that doesn't matter here.

Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

sharness.sh
@@ -20,7 +20,8 @@
 
 if test -n "${ZSH_VERSION-}"
 then
-	SHARNESS_SOURCE=$0
+	# shellcheck disable=SC2296
+	SHARNESS_SOURCE=${(%):-%x}
 	emulate sh -o POSIX_ARGZERO
 else
 	# shellcheck disable=SC3028

commit 37252ec974cce627fdbc5520985a5bfdb5b75d8d

Fix zsh parsing error on NetBSD
The zsh-specific parameter expansion ${(%):-%x} introduced in commit
96e0ea2c7 causes a parse error on NetBSD where /bin/sh cannot parse
this syntax, even though it is inside a zsh-only conditional block.

The error occurs because the shell must parse the entire script
before execution, and NetBSD's /bin/sh fails when it encounters the
zsh-specific syntax with: "Syntax error: Bad substitution".

Wrap the assignment in eval to defer parsing until runtime, when the
script is actually running under zsh.  This maintains the fix for
the NO_FUNCTION_ARGZERO option while restoring compatibility with
NetBSD's /bin/sh.

sharness.sh
@@ -21,7 +21,7 @@
 if test -n "${ZSH_VERSION-}"
 then
 	# shellcheck disable=SC2296
-	SHARNESS_SOURCE=${(%):-%x}
+	eval 'SHARNESS_SOURCE=${(%):-%x}'
 	emulate sh -o POSIX_ARGZERO
 else
 	# shellcheck disable=SC3028